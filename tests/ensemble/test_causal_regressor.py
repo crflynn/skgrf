5cd6c97755d8e3860f136a3ad51b653ca311e762
Flynn <crflynn@users.noreply.github.com>
2021-01-24 14:47:19 -0500

    fix names (#14)

--- tests/ensemble/test_causal_regressor.py
+++ tests/ensemble/test_causal_regressor.py
@@ -5,15 +5,15 @@ from sklearn import clone
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
 
-from skgrf.ensemble import GRFCausal
+from skgrf.ensemble import GRFCausalRegressor
 
 
 class TestGRFCausal:
     def test_init(self):
-        _ = GRFCausal()
+        _ = GRFCausalRegressor()
 
     def test_fit(self, boston_X, boston_y):
-        gfc = GRFCausal()
+        gfc = GRFCausalRegressor()
         with pytest.raises(NotFittedError):
             check_is_fitted(gfc)
         gfc.fit(boston_X, boston_y, boston_y)
@@ -22,14 +22,14 @@ class TestGRFCausal:
         assert hasattr(gfc, "mtry_")
 
     def test_predict(self, boston_X, boston_y):
-        gfc = GRFCausal()
+        gfc = GRFCausalRegressor()
         gfc.fit(boston_X, boston_y, boston_y)
         pred = gfc.predict(boston_X)
         assert len(pred) == boston_X.shape[0]
 
     def test_serialize(self, boston_X, boston_y):
         tf = tempfile.TemporaryFile()
-        gfc = GRFCausal()
+        gfc = GRFCausalRegressor()
         gfc.fit(boston_X, boston_y, boston_y)
         pickle.dump(gfc, tf)
         tf.seek(0)
@@ -38,6 +38,6 @@ class TestGRFCausal:
         assert len(pred) == boston_X.shape[0]
 
     def test_clone(self, boston_X, boston_y):
-        gfc = GRFCausal()
+        gfc = GRFCausalRegressor()
         gfc.fit(boston_X, boston_y, boston_y)
         clone(gfc)
